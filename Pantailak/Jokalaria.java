1c38396d2e8ad5e964ac7f587a8fa20f6d95dcf5
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2025-02-28 13:49:34 +0100

    Update Jokalaria.java

--- Pantailak/Jokalaria.java
+++ Pantailak/Jokalaria.java
@@ -1,5 +1,45 @@
 package Pantailak;
 
 public class Jokalaria {
+  private boolean bizitza;
+	private boolean bonbaPrest; 
+	private int bonbaKop; 
 
+  //Eraikitzailea
+  public Jokalaria (boolean pBizitza, boolean pBonbaPrest, int pBonbaKop) {
+		this.bizitza = true;
+		this.bonbaPrest = true;
+		this.bonbaKop = 10;
+  }
+
+  public void HilEgin () {
+    //TODO
+  }
+
+  public void SpriteAldatu () {
+    //TODO
+  }
+
+  //Posiblea
+  public void BonbaJarri () {
+    if (bonbaPrest && bonbaKop > 0) {
+      //Bonba bota
+      bonbaKop -= 1;
+
+      if (bonbaKop == 0) {
+        bonbaPrest = false;
+      }
+    } 
+  }
+
+  public void BonbaBerria () {
+    if (/*DenboraPasa*/) {
+      bonbaKop += 1;
+      
+      if (!bonbaPrest) {
+        bonbaPrest = true;
+      }
+    }
+  }
+  
 }
