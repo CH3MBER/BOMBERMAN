98b04d2aef36bcbbfac155cb993f09b8fec5fa0d
MikeBarbeQ <76581722+MikeBarbeQ@users.noreply.github.com>
2025-02-28 10:22:03 +0100

    Add files via upload

--- Pantailak/Bloke.java
+++ Pantailak/Bloke.java
@@ -1,5 +1,52 @@
-package Pantailak;
+package proiektua;
 
 public class Bloke {
+	private boolean eztanda; //Blokea eztanda egin al duen ala ez jakiteko
+	private String mota; //Biguna edo ez bereizteko
+	private int posX; //X koordenatuak matrizean jakiteko
+	private int posY; // Y koordenatuak matrizean jakiteko 
+	private boolean dago; //Blokea jokuan jarraitzen duen ala eztanda egin duela jakiteko
+	
+	
+	public Bloke(String pMota, int pPosX, int pPosY ){
+		this.mota= pMota;
+		this.posX=pPosX;
+		this.posY=pPosY;
+		this.dago=true;
+		
+		if (mota.equalsIgnoreCase("Biguna")) { //Blokea biguna denean, eztanda egin ahal duen ala ez jakiteko
+			this.eztanda=true;
+			
+		}else {
+			this.eztanda=false;
+		}
+	}
+	
+	public void eztandaEgin() { //Blokea eztanda egiteko
+		if(eztanda) {
+			this.dago=false;
+			System.out.println("Blokea eztanda egin du.");
+			
+		}else {
+			System.out.println("Blokea ez du eztanda egin.");
+		}
+		
+	}
+	
+	
+	public boolean eztandaEginAlDu() {
+		return eztanda;
+	}
+	public int[] getPosizioa() { //Jakiteko zein posizioan dagoen
+		return new int[] {posX,posY};
+	}
+	
+	public String getMota() {
+		return mota;
+	}
+	
+	public boolean badago() { //Jakiteko jokuan jarraitzen duen ala ez
+		return dago;
+	}
 
 }
--- Pantailak/BlokeZerrenda.java
+++ Pantailak/BlokeZerrenda.java
@@ -1,5 +1,54 @@
-package Pantailak;
+package proiektua;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class BlokeZerrenda {
+	private ArrayList<Bloke> blokeZerrenda;
+	
+	public BlokeZerrenda() {
+		this.blokeZerrenda= new ArrayList<>();
+	}
+	
+	private Iterator<Bloke> getIter(){
+		return this.blokeZerrenda.iterator();
+	}
+	public void gehituBloke(Bloke pBloke) {
+		blokeZerrenda.add(pBloke);
+	}
+	
+	public void deleteBloke(Bloke pBloke) {
+		blokeZerrenda.remove(pBloke);
+	}
+	
+	public Bloke getBloke(int index) {
+		if(index >= 0 && index< blokeZerrenda.size()) {
+			return blokeZerrenda.get(index);
+		}
+		return null;
+	}
+	
+	public ArrayList<Bloke> getBlokeGuztiak(){
+		return new ArrayList<>(blokeZerrenda);
+	}
+	
+	public void eztandaEginPosizioan(int x, int y) {
+		Iterator<Bloke> iter=getIter();
+		while(iter.hasNext()) {
+			Bloke pBloke=iter.next();
+			if (pBloke.getPosizioa()[0]== x&& pBloke.getPosizioa()[1]== y) {
+				pBloke.eztandaEgin();
+				break;
+			}
+		}
+	}
+	
+	public void blokeakInprimatu() {
+		Iterator<Bloke> iter=getIter();
+		while(iter.hasNext()) {
+			Bloke bloke=iter.next();
+			System.out.println("Mota: "+bloke.getMota()+" Posizioa: ("+bloke.getPosizioa()[0]+ ", "+bloke.getPosizioa()[1]+") Dago"+bloke.badago());
+		}
+	}
 
 }
