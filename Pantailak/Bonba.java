53afe7eec4114f8a5e5c1c50f6afb83918d73012
asieerlh <166596846+asieerlh@users.noreply.github.com>
2025-03-03 18:16:56 +0100

    Update Bonba.java

--- Pantailak/Bonba.java
+++ Pantailak/Bonba.java
@@ -1,5 +1,28 @@
 package Pantailak;
 
 public class Bonba {
+	private int radioa;
+	
+	public Bonba(int pRadioa){
+		this.radioa = pRadioa;
+	}
+	
+	public void eztandaEgin() {
+		if (tenp>=3)<{
+			eztanda*this.radioa;
+			deleteBonba();
+		}
+		
+	}
+	
+	private void deleteBonba() {
+		
+		
+	}
+	
+	private int getRadioa() {
+		return this.radioa;
+	}
+
 
 }
