cacfe85ac49aae50b0961b0f782cc3e5a3669a82
asieerlh <166596846+asieerlh@users.noreply.github.com>
2025-03-31 18:10:21 +0200

    Add files via upload

---
+++ Pantailak/EtsaiZerrenda.java
@@ -0,0 +1,53 @@
+package bomberman;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class EtsaiZerrenda {
+	private ArrayList<Etsai> etsaiZerrenda;
+	
+	public EtsaiZerrenda() {
+		etsaiZerrenda = new ArrayList<Etsai>();
+	}
+	
+	private Iterator<Etsai> getIterator(){
+		return this.etsaiZerrenda.iterator();
+		
+	}
+	
+	public void gehituEtsaia(Etsai pEtsai) {
+		this.etsaiZerrenda.add(pEtsai);
+		
+	}
+	
+	public void ezabatuEtsai (int x, int y) {
+		this.etsaiZerrenda.remove(aurkituEtsai(x,y));
+	}
+	
+	public Etsai aurkituEtsai(int x, int y) {
+		boolean aurk = false;
+		Iterator<Etsai> itr = getIterator();
+		Etsai etsaia = null;
+		while (itr.hasNext() && aurk == false) {
+			etsaia = itr.next();
+			if (etsaia.getX() == x && etsaia.getY() == y) {
+				aurk = true;
+			}
+		}
+		if (aurk == false) {
+			etsaia = null;
+		}
+		return etsaia;
+	}
+	
+	public ArrayList<Etsai> erakutsiEtsaiGuztiak(){
+		return new ArrayList<Etsai>(this.etsaiZerrenda);
+	}
+	
+	public int zerrendaTamaina() {
+		return this.etsaiZerrenda.size();
+	}
+	
+
+}
